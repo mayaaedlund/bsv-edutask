52ff9ac14a227695b24a8367fc0a6267acc09528
lohengrin <oljn22@student.bth.se>
2025-04-22 18:29:11 +0200

    Merge Maya and Olofs tests for assignment 2
    From Maya:
    - Imports
    - Fixtures
    - Test 1-3
    
    From Olof:
    - DatabaseException
    - Test 4-5
    
    Updated comments and use of fixtures to sync

---
+++ backend/test/unit/test_get_user_by_email.py
@@ -0,0 +1,102 @@
+# Authors: Maya Edlund and Olof Jönsson
+
+import pytest
+from unittest.mock import MagicMock
+from src.controllers.usercontroller import UserController
+
+pytestmark = pytest.mark.unit
+
+
+class DatabaseException(Exception):
+    """ Custom simulated database exception """
+    pass
+
+
+# Fixtures 
+
+@pytest.fixture
+def valid_email():
+    """ Valid email should contain one '@' and a '.' in the domain. """
+    return "test@test.com"
+
+@pytest.fixture
+def userOne(valid_email):
+    """ Returns a user """
+    return {"email": valid_email, "name": "Single User"}
+
+@pytest.fixture
+def userTwo(valid_email):
+    """ Returns a different user with same email as userOne """
+    return {"email": valid_email, "name": "Second User"}
+
+@pytest.fixture
+def controller():
+    """ Creates a UserController object with mocked DAO. """
+    def _controller(return_value):
+        mock_dao = MagicMock()
+        mock_dao.find.return_value = return_value
+        return UserController(dao=mock_dao)
+    return _controller
+
+
+# Test cases
+
+# Testcase 1: No user found
+# Test fails, code tries to return a user from an empty list!
+def test_valid_email_no_user(controller, valid_email):
+    """ 
+    Testcase when no user is found 
+    Should return None
+    """
+    user_controller = controller([]) # Empty database
+    result = user_controller.get_user_by_email(valid_email)
+    assert result is None
+
+# Testcase 2: one user found
+def test_valid_email_one_user(controller, userOne, valid_email):
+    """ 
+    Testcase when excactly one user is found 
+    Should return user
+    """
+    user_controller = controller([userOne]) # Database with one user. 
+    result = user_controller.get_user_by_email(valid_email)
+    assert result == userOne
+
+# Testcase 3: Two user with same email found
+def test_valid_email_multiple_users(controller, userOne, userTwo, valid_email, capsys):
+    """ 
+    Should return the first user
+    Print a warning message 
+    """
+    user_controller = controller([userOne, userTwo]) # Database with two users
+    result = user_controller.get_user_by_email(valid_email)
+    
+    # Check if first user is returned
+    assert result == userOne
+
+    # Check if warning message is printed. 
+    captured = capsys.readouterr()
+    assert f"more than one user found with mail {valid_email}" in captured.out
+
+# Testcase 4: Database fails (raises DatabaseException)
+def test_valid_email_db_error(valid_email):
+    """ Should raise Exception when database fails """
+    mock_DAO = MagicMock()
+    mock_DAO.find.side_effect = DatabaseException("Error occured in database")  # db error
+    sut = UserController(dao=mock_DAO)
+
+    with pytest.raises(DatabaseException):
+        sut.get_user_by_email(valid_email)
+
+# Testcase 5: Invalid emails
+# Four invalid email formats makes the tests fail, due to a regex that is too simple
+@pytest.mark.parametrize("invalid_email", ["@no.local-part.com", "no.at-symbol.com", "double@@symbols.com", "no@.domain", "no@host"])
+def test_invalid_emails(controller, invalid_email):
+    """
+    Should raise ValueError when email is invalid (all 5 versions)
+    valid email format: <local-part>@<domain>.<host>
+    """
+    sut = controller("A return value") # mocking just to make sure rest of function logic doesnt throw other exceptions
+
+    with pytest.raises(ValueError):
+        sut.get_user_by_email(invalid_email)    # 5 invalid emails
