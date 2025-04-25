3d216155f7d959902530c6b77c94ba65c6ad97b7
lohengrin <oljn22@student.bth.se>
2025-04-25 23:15:16 +0200

    Set up test db and fixtures for DAO:create
    - Add mongodb_test service
    This could maybe be used in a separate docker-compose file instead,
    and the tests could probably be run locally with
    MONGO_URL="mongodb://test:test@localhost:27018"
    
    - Add fixtures
    monkeypatch to set env MONGO_URL
    yield factory function dao
    clean up by droping edutask db
    
    - Add two test
    The tests passed when executed in backend container
    Nothing changed in real mongodb
    State were restored between tests in test mongodb

--- backend/test/integration/test_dao_create_olof.py
+++ backend/test/integration/test_dao_create_olof.py
@@ -1,20 +1,57 @@
 import pytest
+from pymongo import MongoClient
+from pymongo.errors import WriteError
 from unittest.mock import MagicMock
 from src.util.dao import DAO
 
 pytestmark = pytest.mark.integration
 
 @pytest.fixture
-def sut():
-    """  """
-    pass
+def mongo_test_url():
+    return "mongodb://test:test@mongodb_test:27017"
 
-def test_create_video():
-    """ MESSING AROUND """
-    sut = DAO(collection_name="video")
-    video = {"url": "www.test.se"}
+@pytest.fixture
+def mongo_client(mongo_test_url):
+    return MongoClient(mongo_test_url)
 
-    res = sut.create(data=video)
+@pytest.fixture
+def set_MONGO_URL(mongo_test_url, monkeypatch):
+    """ Patch env variable MONGO_URL """
+    monkeypatch.setenv("MONGO_URL", mongo_test_url)
 
-    print(res)
-    assert res["url"] == "www.test.se"
+@pytest.fixture
+def dao(set_MONGO_URL, mongo_client):
+    """ DAO factory with db clean up """
+    def _dao(collection_name):
+        dao = DAO(collection_name=collection_name)
+        return dao
+
+    yield _dao
+
+    mongo_client.drop_database("edutask")
+
+@pytest.fixture
+def valid_task():
+    return {
+        "title": "A title",
+        "description": "A description"
+    }
+
+def test_create_task(dao, valid_task):
+    """ Should create and return a valid task """
+    sut = dao(collection_name="task")
+    res = sut.create(valid_task)
+    assert res["title"] == valid_task["title"]
+
+@pytest.mark.parametrize(
+    "invalid_task",
+    [
+        { "title": "Description is missing (required)" },
+        { "title": "Description has wrong bson type (string expected)", "description": 555 }
+    ]
+)
+def test_create_invalid_tasks(dao, invalid_task):
+    """ Should create and return a valid task """
+    sut = dao(collection_name="task")
+    with pytest.raises(WriteError):
+        sut.create(invalid_task)
