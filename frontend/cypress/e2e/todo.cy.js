4d68bd8fa5e7cd0507c75b6b5b558bce800723bb
mayaaedlund <maya.edlund@hotmail.com>
2025-05-07 12:11:34 +0200

    All testcases in todo.cy.js

--- frontend/cypress/e2e/todo.cy.js
+++ frontend/cypress/e2e/todo.cy.js
@@ -54,6 +54,37 @@ describe("Test CRUD of todo item", () => {
         cy.get("form.inline-form input[type='submit']").click({ force: true });
         cy.get(".todo-list .todo-item").last().should("contain.text", todoText)
     })
+
+    it("TC 2.1 - Toggle to 'done'", () => {
+        cy.contains(taskObj.todos).as("todoDescription").prev().as("toggleIcon")
+        cy.get("@todoDescription").should("not.have.css", "text-decoration-line", "line-through")
+        cy.get("@toggleIcon").click()
+        cy.get("@todoDescription").should("have.css", "text-decoration-line", "line-through")
+    })
+
+    it("TC 2.2 - Toggle to 'active'", () => {
+        // Set todo to 'done' through backend
+        cy.setTodoDone(taskObj, true)
+
+        // Refresh by navigating to detail view again
+        cy.navigateToDetailView(userObj, taskObj)
+
+        cy.contains(taskObj.todos).as("todoDescription").prev().as("toggleIcon")
+        cy.get("@todoDescription").should("have.css", "text-decoration-line", "line-through")
+        cy.get("@toggleIcon").click()
+        cy.get("@todoDescription").should("not.have.css", "text-decoration-line", "line-through")
+    })
+
+    // Fails because TaskDetail does not wait for todo to update done status, but it works when trying manually in browser
+    it("TC 3.1 - Delete todo item", () => {
+        cy.contains(taskObj.todos).as("todoDescription").next().as("deleteBtn")
+        cy.get("@deleteBtn").click()
+        cy.get("@todoDescription").should("not.exist")   // fails, the todo is still there
+
+        // This works though. cy.wait() does not solve it
+        // cy.navigateToDetailView(userObj, taskObj)
+        // cy.contains(taskObj.todos).should("not.exist")
+    })
     
 
     after(function () {
